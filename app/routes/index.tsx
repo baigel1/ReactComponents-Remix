d1820dfe1f1c6c43da37e98c4d879d9506881082
Daniel <dbaigel@yext.com>
2022-05-20 16:20:47 -0400

    testing fly

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,20 +1,21 @@
 import { Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
-import { SearchBar, UniversalResults, StandardCard } from "@yext/answers-react-components";
+//import { SearchBar, UniversalResults, StandardCard } from "@yext/answers-react-components";
 
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <div>
-      <SearchBar />
+    <div className="text-red-500">
+      this is my app!
+      {/* <SearchBar />
       <UniversalResults
         verticalConfigMap={{
           products: {
             CardComponent: ({ result }) => <StandardCard result={result} />,
           },
         }}
-      />
+      /> */}
     </div>
   );
 }
